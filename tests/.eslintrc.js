17c8fc11e37fcc3ee3dce985f2611c7e4bda5cf5
Jan Buschtöns <jan@buschtoens.me>
2020-08-03 14:56:01 +0200

    chore: fix linting setup

--- tests/.eslintrc.js
+++ tests/.eslintrc.js
@@ -1,6 +1,10 @@
-/* eslint-env node */
-
 module.exports = {
   root: true,
-  extends: '@clark/ember-typescript'
+  extends: '@clark/ember-typescript/test',
+  rules: {
+    // https://github.com/ember-cli/eslint-plugin-ember/issues/905
+    'ember/no-new-mixins': 'off',
+    'ember/no-attrs-in-components': 'off',
+    'ember/no-get': 'off'
+  }
 };
